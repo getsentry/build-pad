1749af02d23bb64b8ea227bf1f2ec4afe41a61a4
Anton Ovchinnikov <anton@tonyo.info>
2019-03-05 17:32:49 +0100

    feat: Build Crashpad with Sentry patches

---
+++ crashpad/fetch.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -eux
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+
+mkdir -p build
+cd build
+
+# Install depot_tools
+if [ -d depot_tools ]; then
+  pushd depot_tools
+  git pull -r
+  popd
+else
+  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+
+. $SCRIPT_DIR/vars.sh
+
+# Checkout and sync crashpad
+if [ -d crashpad ]; then
+  cd crashpad
+  $GCLIENT_CMD sync
+  git fetch origin
+  git rebase origin/master
+else
+  $FETCH_CMD crashpad
+  cd crashpad
+  # Apply Sentry patches
+  git checkout -b feat/getsentry-patches
+  git am -3 --ignore-whitespace --ignore-space-change < $SCRIPT_DIR/patches/getsentry.patch
+fi
